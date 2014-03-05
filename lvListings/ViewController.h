911098a3cf78e8368c65b678e5f76c24b66aaacc
Daniel Lagazo <daniel@Daniels-Mac-mini.local>
2014-03-05 18:29:15 +0800

    Initial Commit

---
+++ lvListings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lvListings
+//
+//  Created by Daniel Lagazo on 3/5/14.
+//  Copyright (c) 2014 Daniel Lagazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ lvListingsTests/lvListingsTests.m
@@ -0,0 +1,34 @@
+//
+//  lvListingsTests.m
+//  lvListingsTests
+//
+//  Created by Daniel Lagazo on 3/5/14.
+//  Copyright (c) 2014 Daniel Lagazo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lvListingsTests : XCTestCase
+
+@end
+
+@implementation lvListingsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
